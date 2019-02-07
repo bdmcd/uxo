f99ddc32975fd8767ca13261afef7a5d40326e35
Brian McDaniel <brianmcdaniel47@gmail.com>
2019-02-07 11:15:24 -0700

    Changes made to get the app to build for android with Qt 5.12

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
@@ -38,6 +39,9 @@ int main(int argc, char *argv[])
     engine.rootContext()->setContextProperty("Color", &color);
 
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
     return app.exec();
 }
 
